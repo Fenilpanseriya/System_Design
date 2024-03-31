833af4ee337e7a01f69c84b9df118a776d415362
Fenilpanseriya <fenilpanseriya2004@gmail.com>
2024-03-31 16:36:23 +0530

    Add requirements for Design a Bird problem

--- Design-Bird/Main.java
+++ Design-Bird/Main.java
@@ -1,7 +1,15 @@
+//Requirements
+// there should be 5 birds sparrow,eagle,crow,penguin,pigeon
+//1. Flying behavior of sparrow and pigeon is same
+//2. Flying behavior of Eagle and crow is same
+//3. Penguin can not fly
+//4. all birds can eat
+// 
+
 public class Main {
     public static void main(String[] args) {
         Penguin p=new Penguin();
-        
+
         p.eat();
         p.notfly();
 
